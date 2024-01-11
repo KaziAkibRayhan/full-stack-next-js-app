cd7a8907ed93c41e3138a43eeebd50dc162bcf30
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2024-01-12 02:00:49 +0600

    created navbar and adddedloading state

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+
+const Links = () => {
+  const links = [
+    { title: "HomePage", path: "/" },
+    { title: "About", path: "/about" },
+    { title: "Contact", path: "/contact" },
+    { title: "Blog", path: "/blog" },
+  ];
+
+  return (
+    <div>
+      {links.map((link) => (
+        <Link key={link.title} href={link.path}>
+          {link.title}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Links;
